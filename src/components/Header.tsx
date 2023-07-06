5012a03d7ecf14b704520a8d41a776a991e85211
Micky Rajkumar <pathourajkumar@gmail.com>
2023-07-06 18:37:26 +0530

    fixed link issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,22 +5,23 @@ export const Header: FC = () => {
     <header className="fixed w-full z-[9999]">
       <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
         <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-          <Link href="#" className="flex items-center">
-            <img
-              src="./nextjs.png"
-              className="h-6 mr-3 sm:h-9"
-              alt="Landwind Logo"
-            />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              NextJs
-            </span>
+          <Link href="https://nextjs.org" className="flex items-center">
+            <a>
+              <img
+                src="./nextjs.png"
+                className="h-6 mr-3 sm:h-9"
+                alt="Landwind Logo"
+              />
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                NextJs
+              </span>
+            </a>
           </Link>
           <div className="flex items-center lg:order-2">
-            <Link
-              href="#"
-              className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
-            >
-              Download
+            <Link href="/download">
+              <a className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
+                Download
+              </a>
             </Link>
             <button
               data-collapse-toggle="mobile-menu-2"
@@ -29,7 +30,7 @@ export const Header: FC = () => {
               aria-controls="mobile-menu-2"
               aria-expanded="false"
             >
-              <span className="sr-only">Open main menu</span>
+              <span className="sr-only">Menu</span>
               <svg
                 className="w-6 h-6"
                 fill="currentColor"
@@ -63,51 +64,51 @@ export const Header: FC = () => {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <Link
-                  href="#"
+                  href="/"
                   className="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
                   aria-current="page"
                 >
-                  Home
+                  <a>Home</a>
                 </Link>
               </li>
               <li>
                 <Link
-                  href="#"
+                  href="/company"
                   className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Company
+                  <a>Company</a>
                 </Link>
               </li>
               <li>
                 <Link
-                  href="#"
+                  href="/market-place"
                   className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Marketplace
+                  <a>Marketplace</a>
                 </Link>
               </li>
               <li>
                 <Link
                   href="/feature"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 pl-3 pr-4 cursor-pointer text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Features
+                  <a>Features</a>
                 </Link>
               </li>
               <li>
                 <Link
-                  href="#"
+                  href="/team"
                   className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Team
+                  <a>Team</a>
                 </Link>
               </li>
               <li>
                 <Link
-                  href="#"
+                  href="/contact"
                   className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Contact
+                  <a>Contact</a>
                 </Link>
               </li>
             </ul>
